5d6e5bb29a6b2ea664f414b34d125d1f99d071fa
nymphadora07 <cocnymphadora@gmail.com>
2026-08-31 16:02:21 +0700

    fix: require breadcrumb.php

--- functions.php
+++ functions.php
@@ -14,4 +14,5 @@ require get_template_directory() . '/inc/google-fonts.php';
 require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/game-context.php';
 require get_template_directory() . '/inc/game-queries.php';
+require get_template_directory() . '/inc/breadcrumb.php';
 require get_template_directory() . '/inc/enqueue.php';

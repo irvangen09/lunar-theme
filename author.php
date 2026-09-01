85d77daa6e2e6606f5da22a73987f53433db9336
nymphadora07 <cocnymphadora@gmail.com>
2026-09-01 17:04:38 +0700

    feat: port author archive template

---
+++ author.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @package Lunar
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+
+$lunar_author_id = get_queried_object_id();
+
+lunar_breadcrumb();
+?>
+
+<main id="main-content" class="lunar-archive">
+
+	<header class="lunar-archive__header lunar-archive__header--author">
+		<?php lunar_render_author_box( $lunar_author_id, true ); ?>
+	</header>
+
+	<?php if ( have_posts() ) : ?>
+
+		<div class="lunar-archive-list">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				$lunar_content_type_slug  = function_exists( 'lunar_wiki_get_taxonomy_slug_content_type' )
+					? lunar_wiki_get_taxonomy_slug_content_type()
+					: '';
+				$lunar_content_type_terms = $lunar_content_type_slug
+					? get_the_terms( get_the_ID(), $lunar_content_type_slug )
+					: false;
+				$lunar_article_game_term  = null;
+
+				if ( function_exists( 'lunar_wiki_get_taxonomy_slug_game' ) ) {
+					$lunar_article_game_terms = get_the_terms( get_the_ID(), lunar_wiki_get_taxonomy_slug_game() );
+
+					if ( is_array( $lunar_article_game_terms ) && ! empty( $lunar_article_game_terms ) ) {
+						$lunar_article_game_term = $lunar_article_game_terms[0];
+					}
+				}
+				?>
+				<div class="lunar-archive-list-item">
+					<?php if ( is_array( $lunar_content_type_terms ) && ! empty( $lunar_content_type_terms ) ) : ?>
+						<span class="lunar-archive-list-item__badge">
+							<?php echo esc_html( $lunar_content_type_terms[0]->name ); ?>
+						</span>
+					<?php endif; ?>
+					<a class="lunar-archive-list-item__title" href="<?php the_permalink(); ?>">
+						<?php the_title(); ?>
+						<?php if ( $lunar_article_game_term ) : ?>
+							<span class="lunar-archive-list-item__game">(<?php echo esc_html( $lunar_article_game_term->name ); ?>)</span>
+						<?php endif; ?>
+					</a>
+				</div>
+				<?php
+			endwhile;
+			?>
+		</div>
+
+		<?php the_posts_pagination(); ?>
+
+	<?php else : ?>
+
+		<p><?php esc_html_e( 'Belum ada artikel dari penulis ini.', 'lunar' ); ?></p>
+
+	<?php endif; ?>
+
+</main>
+
+<?php
+get_footer();

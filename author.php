e2cd0f590bb5ef65bcd84890ad269145ba38a83e
nymphadora07 <cocnymphadora@gmail.com>
2026-09-08 05:53:07 +0700

    refactor: use shared archive-list-item helper instead of duplicated markup

--- author.php
+++ author.php
@@ -26,37 +26,7 @@ lunar_breadcrumb();
 			<?php
 			while ( have_posts() ) :
 				the_post();
-
-				$lunar_content_type_slug  = function_exists( 'lunar_wiki_get_taxonomy_slug_content_type' )
-					? lunar_wiki_get_taxonomy_slug_content_type()
-					: '';
-				$lunar_content_type_terms = $lunar_content_type_slug
-					? get_the_terms( get_the_ID(), $lunar_content_type_slug )
-					: false;
-				$lunar_article_game_term  = null;
-
-				if ( function_exists( 'lunar_wiki_get_taxonomy_slug_game' ) ) {
-					$lunar_article_game_terms = get_the_terms( get_the_ID(), lunar_wiki_get_taxonomy_slug_game() );
-
-					if ( is_array( $lunar_article_game_terms ) && ! empty( $lunar_article_game_terms ) ) {
-						$lunar_article_game_term = $lunar_article_game_terms[0];
-					}
-				}
-				?>
-				<div class="lunar-archive-list-item">
-					<?php if ( is_array( $lunar_content_type_terms ) && ! empty( $lunar_content_type_terms ) ) : ?>
-						<span class="lunar-archive-list-item__badge">
-							<?php echo esc_html( $lunar_content_type_terms[0]->name ); ?>
-						</span>
-					<?php endif; ?>
-					<a class="lunar-archive-list-item__title" href="<?php the_permalink(); ?>">
-						<?php the_title(); ?>
-						<?php if ( $lunar_article_game_term ) : ?>
-							<span class="lunar-archive-list-item__game">(<?php echo esc_html( $lunar_article_game_term->name ); ?>)</span>
-						<?php endif; ?>
-					</a>
-				</div>
-				<?php
+				lunar_render_archive_list_item();
 			endwhile;
 			?>
 		</div>
